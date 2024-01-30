c7d04ffa578413cb037d4ded007aef601d273be6
Emam Hossain <eh.tamim1708@gmail.com>
2024-01-30 11:47:09 +0600

    Initial

---
+++ start.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class start {
+   public static void main(String[] args) {
+      PrintStream ps = new PrintStream(System.out);
+      ps.print("Please select an input");
+   }
+}
